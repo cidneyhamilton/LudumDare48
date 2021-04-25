dbdf09648edbacf8d083db3cc57b3437dae3815c
Cidney Hamilton <cidney@cidneyhamilton.com>
2021-04-25 10:31:40 -0400

    Add sliders to control the volume of the game.

---
+++ Assets/Scripts/Settings/Slider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cyborg.Clinic {
+
+    // A slider with two buttons to toggle up and down
+    public abstract class Slider : MonoBehaviour
+    {
+
+        public UnityEngine.UI.Slider slider;
+
+        public virtual void Up() {
+            if (slider != null) {
+                slider.value = Mathf.Clamp(slider.value + 1f, slider.minValue, slider.maxValue); 
+            }
+        }
+
+        public virtual void Down() {
+            if (slider != null) {
+                slider.value = Mathf.Clamp(slider.value - 1f, slider.minValue, slider.maxValue); 
+            }
+            
+        }
+    }
+
+}
---
+++ Assets/Scripts/Settings/VolumeSlider.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using Cyborg.Audio;
+
+namespace Cyborg.Clinic {
+
+    // Slider to adjust the in-game volume
+    public class VolumeSlider : Slider
+    {
+
+        const float SCALE_FACTOR = 10f;
+        public AudioMixer volumeMixer;
+	const string VOLUME = "volume";
+	float mixerVolume;
+	
+        void Start()
+        {
+	    
+	    volumeMixer.GetFloat(VOLUME, out mixerVolume);
+            slider.value = VolumeToSliderValue(mixerVolume);
+        }
+
+	private float VolumeToSliderValue(float volume) {
+	    return Mathf.Pow(10, volume/20);
+	}
+
+	private float SliderValueToVolume(float value) {
+	    return Mathf.Log10(value) * 20;
+	}
+	
+        public void UpdateSlider() {
+            if (slider != null) {
+                volumeMixer.SetFloat(VOLUME, SliderValueToVolume(slider.value));
+            }
+        }
+
+        public override void Up() {
+            base.Up();
+            UpdateSlider();
+
+            AudioEvents.PlaySound("click");
+        }
+
+        public override void Down() {
+            base.Down();
+            UpdateSlider();
+
+	    AudioEvents.PlaySound("click");            
+        }
+    }
+
+}
